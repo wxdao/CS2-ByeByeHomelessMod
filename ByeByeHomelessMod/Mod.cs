cabd9890263ed5e9a7f91cfb0747f53e7a88645c
wxdao <waxiadao@gmail.com>
2024-07-06 21:37:14 +0800

    Initial mod code

---
+++ ByeByeHomelessMod/Mod.cs
@@ -0,0 +1,136 @@
+﻿using System.Runtime.CompilerServices;
+using Colossal.Logging;
+using Game;
+using Game.Agents;
+using Game.Buildings;
+using Game.Citizens;
+using Game.City;
+using Game.Common;
+using Game.Economy;
+using Game.Modding;
+using Game.Prefabs;
+using Game.SceneFlow;
+using Game.Simulation;
+using Game.Tools;
+using Game.Triggers;
+using Game.Vehicles;
+using Unity.Burst.Intrinsics;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine.Scripting;
+using Unity.Mathematics;
+
+namespace ByeByeHomelessMod
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(ByeByeHomelessMod)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            updateSystem.UpdateAt<ByeByeHomelessSystem>(SystemUpdatePhase.GameSimulation);
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+
+
+    public partial class ByeByeHomelessSystem : GameSystemBase
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(ByeByeHomelessMod)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        private struct ByeByeHomelessJob : IJobChunk
+        {
+            [ReadOnly]
+            public EntityTypeHandle m_EntityType;
+
+            public EntityCommandBuffer.ParallelWriter m_CommandBuffer;
+
+            public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+            {
+                NativeArray<Entity> nativeArray = chunk.GetNativeArray(m_EntityType);
+                for (int i = 0; i < nativeArray.Length; i++)
+                {
+                    Entity entity = nativeArray[i];
+                    m_CommandBuffer.AddComponent(unfilteredChunkIndex, entity, default(Deleted));
+                }
+            }
+
+            void IJobChunk.Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+            {
+                Execute(in chunk, unfilteredChunkIndex, useEnabledMask, in chunkEnabledMask);
+            }
+        }
+
+        private struct TypeHandle
+        {
+            [ReadOnly]
+            public EntityTypeHandle __Unity_Entities_Entity_TypeHandle;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void __AssignHandles(ref SystemState state)
+            {
+                __Unity_Entities_Entity_TypeHandle = state.GetEntityTypeHandle();
+            }
+        }
+
+        private EntityQuery m_HomelessGroup;
+
+        private EndFrameBarrier m_EndFrameBarrier;
+
+        private TypeHandle __TypeHandle;
+
+        public override int GetUpdateInterval(SystemUpdatePhase phase)
+        {
+            return 16;
+        }
+
+        [Preserve]
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            m_EndFrameBarrier = base.World.GetOrCreateSystemManaged<EndFrameBarrier>();
+            m_HomelessGroup = GetEntityQuery(ComponentType.ReadOnly<Household>(), ComponentType.ReadOnly<HouseholdCitizen>(), ComponentType.Exclude<CommuterHousehold>(), ComponentType.Exclude<TouristHousehold>(), ComponentType.Exclude<PropertyRenter>(), ComponentType.Exclude<Deleted>(), ComponentType.Exclude<Temp>());
+            RequireForUpdate(m_HomelessGroup);
+        }
+
+        [Preserve]
+        protected override void OnUpdate()
+        {
+            __TypeHandle.__Unity_Entities_Entity_TypeHandle.Update(ref base.CheckedStateRef);
+            ByeByeHomelessJob byeByeHomelessJob = default(ByeByeHomelessJob);
+            byeByeHomelessJob.m_EntityType = __TypeHandle.__Unity_Entities_Entity_TypeHandle;
+            byeByeHomelessJob.m_CommandBuffer = m_EndFrameBarrier.CreateCommandBuffer().AsParallelWriter();
+            ByeByeHomelessJob jobData = byeByeHomelessJob;
+            base.Dependency = JobChunkExtensions.ScheduleParallel(jobData, m_HomelessGroup, base.Dependency);
+            m_EndFrameBarrier.AddJobHandleForProducer(base.Dependency);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void __AssignQueries(ref SystemState state)
+        {
+        }
+
+        protected override void OnCreateForCompiler()
+        {
+            base.OnCreateForCompiler();
+            __AssignQueries(ref base.CheckedStateRef);
+            __TypeHandle.__AssignHandles(ref base.CheckedStateRef);
+        }
+
+        [Preserve]
+        public ByeByeHomelessSystem()
+        {
+        }
+    }
+}
